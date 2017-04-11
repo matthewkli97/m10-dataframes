4c074e8ab25473ad15a4759124f1f881a672490b
Matthew Li <matthewli@D-173-250-149-56.dhcp4.washington.edu>
2017-04-11 14:49:35 -0700

    exercise 3 complete

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,37 +2,46 @@
 
 # Load R's "USPersonalExpenditure" dataest using the `data()` function
 
+data("USPersonalExpenditure")
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately, it's not a data.frame
 # Test this using the is.data.frame function
 
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can simply pass the USPersonalExpenditure variable to the data.frame function
 # to convert it a data.farme
 
 # Create a new variable by passing the USPersonalExpenditure to the data.frame function
 
+new.data <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
 
+print(colnames(new.data))
 
 # Why are they so strange?
 
-
 # What are the row names of your dataframe?
 
+print(rownames(new.data))
 
 # Create a column `category` that is equal to your rownames
 
+row.name <- rownames(new.data)
+new.data$category <- row.name
 
 # How much money was spent on personal care in 1940?
 
+total.personal <- sum(new.data[4])
 
 # How much money was spent on Food and Tobacco in 1960
 
+total.food <- sum(new.data[1])
 
 # What was the highest expenditure category in 1960?
 
+totals <- sum(c(new.data(1:6)))
 
 ### Bonus ###
 
